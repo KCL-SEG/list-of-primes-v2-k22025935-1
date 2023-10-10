c149cbfe579a7ce592708cb4d24e4e502e219c8f
Caleb Chan <caleb.chan@kcl.ac.uk>
2023-10-10 19:23:50 +0100

    First Solution

--- primes.py
+++ primes.py
@@ -1,6 +1,19 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
+def isPrime(num):
+    for i in range (2,num):
+        if num%i == 0:
+            return False
+    return True
 
 def primes(number_of_primes):
-    list = []
-    return list
+    if number_of_primes<1:
+        raise ValueError
+    else:
+        list = []
+        num = 2
+        while len(list)<number_of_primes:
+            if isPrime(num):
+                list.append(num)
+            num+=1
+        return list
